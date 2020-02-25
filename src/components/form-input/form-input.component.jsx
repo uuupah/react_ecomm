b5b7d2e341bac014b734660e2a0862ec1b1a234f
uuupah <uuupah@gmail.com>
2020-02-25 20:06:10 +1030

    initialise sign in and sign up page, and implement simple sign in component

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./form-input.styles.scss";
+
+const FormInput = ({ handleChange, label, ...otherProps }) => (
+  <div className="group">
+    <input className="form-input" onChange={handleChange} {...otherProps} />
+    {// if label exists, create label, if not do nothing
+    label ? (
+      <label
+        className={`${
+          otherProps.value.length ? "shrink" : ""
+        } form-input-label`}
+      >
+        {label}
+      </label>
+    ) : null}
+  </div>
+);
+
+export default FormInput;
